5d500a47731fb2a60f89d214ec5695dd5b56f2b3
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-18 21:51:55 +0500

    Add project files.

---
+++ Infinite Series ex/Program.cs
@@ -0,0 +1,26 @@
+﻿class InfiniteSeries
+{
+    static void Main(string[] args)
+    {
+        double ex = 1.0;
+        double denominator = 1.0;
+        double numerator = 1.0;
+
+        Console.Write("Enter The Number Of Terms To Be Used In The Series:  ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter The Value Of X:  ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        for (int i = 1; i <= number; i++) 
+        {
+            numerator *= x;
+            denominator *= i;
+            ex = numerator / denominator;
+        }
+
+        Console.WriteLine("Estimated Value Of e^x:  " + ex);
+
+        Console.WriteLine("Actual Value Of e^x:  " + Math.Pow(Math.E, x));
+    }
+}
